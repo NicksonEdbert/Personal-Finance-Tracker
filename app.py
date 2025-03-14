6455944063d406247615df2c863a04975ee82c79
NicksonEdbert <nicksonedbert@gmail.com>
2025-03-14 13:30:03 -0700

    added the rest of the tables.

--- app.py
+++ app.py
@@ -26,6 +26,31 @@ class User(db.Model):
         return f"<User {self.FirstName} {self.LastName}>"
 
 
+class Transaction(db.Model):
+    __tablename__ = 'Transactions'
+    TransactionID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer, nullable=False)
+    CategoryID = db.Column(db.Integer, nullable=False)
+    Amount = db.Column(db.Integer, nullable=False)
+    TransactionType = db.Column(db.Enum('Income', 'Expense'), nullable=False)
+    TransactionDate = db.Column(db.DateTime, nullable=False)
+    Description = db.Column(db.Text)
+
+    def __repr__(self):
+        return (f"<Transaction ID: {self.TransactionID}, "
+                f"Category: {self.CategoryID}, Amount: {self.Amount}, "
+                f"Type: {self.TransactionType}>")
+
+
+class Category(db.Model):
+    __tablename__ = 'Categories'
+    CategoryID = db.Column(db.Integer, primary_key=True)
+    CategoryName = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f"<Category {self.CategoryID}: {self.CategoryName}>"
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -54,7 +79,7 @@ def add_user():
         db.session.commit()
 
         return redirect(url_for('get_users'))
-    
+
     # For GET request, display the form
     return render_template('add_user.html')
 
